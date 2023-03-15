a7564a7c0da7eecadcf933be4d6837cee16c2cda
Arush99 <105558684+Arush99@users.noreply.github.com>
2023-03-15 20:00:35 +0530

    3rd

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:8000";
+const URL = "https://flipkart-ecomm.adaptable.app/";
 
 export const authenticateSignup = async (data) => {
   try {
